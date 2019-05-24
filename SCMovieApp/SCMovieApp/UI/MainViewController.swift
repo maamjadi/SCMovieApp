c4b50c1c53fe9f67ff92ba618692ad5a7f2df714
Amin Amjadi <ma.amjadi@icloud.com>
2019-05-24 19:46:54 +0200

    main screen created

---
+++ SCMovieApp/SCMovieApp/UI/MainViewController.swift
@@ -0,0 +1,77 @@
+//
+//  MainViewController.swift
+//  SCMovieApp
+//
+//  Created by Amin Amjadi on 2019. 05. 24..
+//  Copyright © 2019. Amin Amjadi. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var navigationTitle: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    private let cellIdentifier = "movieCell"
+    private let searchController = UISearchController(searchResultsController: nil)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "MovieTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: cellIdentifier)
+        navigationController?.navigationBar.shadowImage = UIImage()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        navigationTitle.textColor = UIColor.greyishBrown()
+    }
+
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Candies"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
+
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+}
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+            as? MovieTableViewCell else {
+            return MovieTableViewCell()
+        }
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+extension MainViewController: UISearchBarDelegate {
+    // MARK: - UISearchBar Delegate
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+    }
+}
+
+extension MainViewController: UISearchResultsUpdating {
+    // MARK: - UISearchResultsUpdating Delegate
+    func updateSearchResults(for searchController: UISearchController) {
+    }
+}
