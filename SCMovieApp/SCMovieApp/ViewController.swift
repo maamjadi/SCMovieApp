2079be07a23b02c85e44231566740264d15a678e
Amin Amjadi <ma.amjadi@icloud.com>
2019-05-24 18:16:29 +0200

    initial commit

---
+++ SCMovieApp/SCMovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SCMovieApp
+//
+//  Created by Amin Amjadi on 2019. 05. 24..
+//  Copyright © 2019. Amin Amjadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
